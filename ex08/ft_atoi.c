ba1d778b00297c2de3ddb4cac6073b302a57d548
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-19 11:46:13 +0200

    Finished ex08

---
+++ ex08/ft_atoi.c
@@ -0,0 +1,22 @@
+
+int		ft_atoi(char *str)
+{
+	int		i;
+	int		sign;
+	int		nbr;
+
+	i = 0;
+	sign = 1;
+	nbr = 0;
+	if (!str[i])
+		return (0);
+	while (c == '\t' || c == '\v' || c == '\n' || c == '\r' \
+		|| c == '\f' || c == ' ')
+		i += 1;
+	if (str[i] == '-' || str[i] == '+')
+		if (str[i++] == '-')
+			sign = -1;
+	while (str[i] >= '0' && str[i] <= '9')
+		nbr = (nbr * 10) + (str[i++] - '0');
+	return (nbr * sign);
+}
