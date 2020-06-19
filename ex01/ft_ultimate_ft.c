dade298771758bf9f687e04061a78611886233bb
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-19 11:10:11 +0200

    Finished Ex01

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,6 @@
+/*The function that takes a pointer to pointer to pointer to pointer to pointerto pointer to pointer to pointer to pointer to int as a parameter and sets the value"42" to that int.*/
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
