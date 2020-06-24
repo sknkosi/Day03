880b4ed2d56d02b486ab71a64f1316ddb081b378
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-24 16:50:12 +0200

    Finished Ex00

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -6,13 +7,24 @@
 /*   By: sknkosi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 09:29:29 by sknkosi           #+#    #+#             */
-/*   Updated: 2020/06/24 10:01:27 by sknkosi          ###   ########.fr       */
+/*   Updated: 2020/06/24 15:56:58 by sknkosi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
 /*The function takes a pointer to int as a parameter, and sets the value "42"to that int*/
 
 void ft_ft(int *nbr)
 {
-	*nbr = 42;
+	nbr = 42;
+	printf("%d",nbr);
 }
+
+int main()
+{
+	int *n;
+	n = 42;
+	ft_ft(&n);
+	return 0;
+}
+
