8b93bd3314f3bba0897ed9d3a3cc4469aa2f4e9a
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-24 09:31:28 +0200

    Finished Ex01

--- ex01/ft_ultimate_ft.c
+++ ex01/ft_ultimate_ft.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sknkosi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/24 09:30:59 by sknkosi           #+#    #+#             */
+/*   Updated: 2020/06/24 09:31:03 by sknkosi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*The function that takes a pointer to pointer to pointer to pointer to pointerto pointer to pointer to pointer to pointer to int as a parameter and sets the value"42" to that int.*/
 
 void ft_ultimate_ft(int *********nbr)
