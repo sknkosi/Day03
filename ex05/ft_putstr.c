1c9953c713165796bd90661cdb9cc8d61c4086c0
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-24 09:39:59 +0200

    Finished Ex05

--- ex05/ft_putstr.c
+++ ex05/ft_putstr.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sknkosi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/24 09:39:15 by sknkosi           #+#    #+#             */
+/*   Updated: 2020/06/24 09:39:37 by sknkosi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*The function ptints a string of characters on a standard output.*/
 
 int	ft_putchar(char c);
@@ -5,7 +17,6 @@ int	ft_putchar(char c);
 void ft_putstr(char *str)
 {
 	int i;
-
 	i = 0;
 	while (str[i])
 		ft_putchar(str[i++]);
