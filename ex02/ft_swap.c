0e84f94158135a7d252663daea57f5e20ead858b
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-25 12:48:15 +0200

    Finished Ex02

--- ex02/ft_swap.c
+++ ex02/ft_swap.c
@@ -6,11 +6,10 @@
 /*   By: sknkosi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 09:32:52 by sknkosi           #+#    #+#             */
-/*   Updated: 2020/06/25 10:02:05 by sknkosi          ###   ########.fr       */
+/*   Updated: 2020/06/25 12:47:57 by sknkosi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
-#include <stdio.h>
+
 /*The function swaps value of two integers whose addresses are entered as parameters*/
 
 void ft_swap(int *a, int *b)
