e2b5d5a4f0dd0a9478318a68e7398cb952a06bdd
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-25 11:56:19 +0200

    Finished Ex04

--- ex04/ft_ultimate_div_mod.c
+++ ex04/ft_ultimate_div_mod.c
@@ -6,16 +6,20 @@
 /*   By: sknkosi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 09:37:52 by sknkosi           #+#    #+#             */
-/*   Updated: 2020/06/24 09:38:08 by sknkosi          ###   ########.fr       */
+/*   Updated: 2020/06/25 11:55:52 by sknkosi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
 
 void ft_ultimate_div_mod(int *a, int *b)
 {
-	int tmp;
+	int tmp1;
+	int tmp2;
 
-	*tmp = *a;
-	*a /= *b;
-	*b = *tmp % b;
+	tmp1 = *a / *b;
+	tmp2 = a* % *b;
+	*a = tmp1;
+	*b = tmp2;
 }
