2251a4ef54980043cf8f8e71dc9e173496a937bc
Skhumbuzo Nkosi <sknkosi@c4r16s1.wethinkcode.co.za>
2020-06-25 12:37:31 +0200

    Finished Ex08

--- ex08/ft_atoi.c
+++ ex08/ft_atoi.c
@@ -6,28 +6,35 @@
 /*   By: sknkosi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 09:46:07 by sknkosi           #+#    #+#             */
-/*   Updated: 2020/06/24 09:46:17 by sknkosi          ###   ########.fr       */
+/*   Updated: 2020/06/25 12:37:00 by sknkosi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_atoi(char *str)
-{
-	int		i;
-	int		sign;
-	int		nbr;
-
-	i = 0;
-	sign = 1;
-	nbr = 0;
-	if (!str[i])
-		return (0);
-	while (c == '\t' || c == '\v' || c == '\n' || c == '\r' \
-		|| c == '\f' || c == ' ')
-		i += 1;
-	if (str[i] == '-' || str[i] == '+')
-		if (str[i++] == '-')
-			sign = -1;
-	while (str[i] >= '0' && str[i] <= '9')
-		nbr = (nbr * 10) + (str[i++] - '0');
-	return (nbr * sign);
-}
+int  ft_atoi(char *str)
+  {
+      int res;
+      int sign;
+      int i;
+ 
+      res = 0;
+      sign = 0;
+      i = 0;
+ 
+      while ((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n') || (str[i] == '\v') || (str[i] == '\r') || (str[i] == '\f'))
+          i++;
+      if (str[i] == '-')
+          sign = 1;
+      if (str[i] == '+' || str[i] == '-')
+          i++;
+ 
+          while (str[i] && (str[i] >= '0') && (str[i] <= '9'))
+          {
+              res = res * 10;
+              res += (int)str[i] - '0';
+              i++;
+          }
+          if (sign == 1)
+             return (-res);
+          else
+             return (res);
+  }
